2acf923d49372d5cf488ce2a6ffc6c10a3099693
jacsonikowy <michal6343@wp.pl>
2023-02-21 14:58:50 +0100

    fix: redux, authoriztion

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,16 +1,46 @@
 import CityInput from "components/CityInput/CityInput";
 import React, { useState } from "react";
+import { useNavigate } from "react-router";
 import styles from "./Home.module.scss";
+
+import { ReactComponent as Celsius } from "assets/icons/celsius.svg";
+import { ReactComponent as Fahrenheit } from "assets/icons/fahrenheit.svg";
+import Weather from "components/Weather/Weather";
+import FavoriteCity from "components/FavoriteCity/FavoriteCity";
 
 const Home: React.FC = () => {
   const [celsius, setCelsius] = useState(true);
+  const [cityName, setCityName] = useState("");
+
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.setItem("user", "");
+    navigate("/");
+  };
+
+  if (localStorage.getItem("user") === "") {
+    return <div>UNAUTHORIZED</div>;
+  }
 
   return (
     <div className={styles.home}>
-      <button onClick={() => setCelsius(!celsius)}>
-        {celsius ? "Celsius" : "Fahrenheit"}
-      </button>
-      <CityInput />
+      <div>
+        <button onClick={handleLogout}>Log Out</button>
+        <button
+          className={styles.temperatureBtn}
+          onClick={() => setCelsius(!celsius)}
+        >
+          {celsius ? <Celsius /> : <Fahrenheit />}
+        </button>
+      </div>
+      <div className={styles.weatherInfo}>
+        <CityInput setCityName={setCityName} />
+        <Weather cityName={cityName} />
+      </div>
+      <div className={styles.favorites}>
+        <FavoriteCity cityName="Warsaw" />
+      </div>
     </div>
   );
 };
