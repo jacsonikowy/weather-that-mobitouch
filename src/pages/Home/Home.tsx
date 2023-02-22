b7ed2279a99d8440bb7196f63c86e9021a0ac42e
jacsonikowy <michal6343@wp.pl>
2023-02-22 13:59:28 +0100

    fix(home): dispatching while logging out and css change

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,22 +1,32 @@
 import CityInput from "components/CityInput/CityInput";
 import React, { useState } from "react";
 import { useNavigate } from "react-router";
+import { useDispatch } from "react-redux";
 import styles from "./Home.module.scss";
 
 import { ReactComponent as Celsius } from "assets/icons/celsius.svg";
 import { ReactComponent as Fahrenheit } from "assets/icons/fahrenheit.svg";
 import Weather from "components/Weather/Weather";
 import FavoriteCity from "components/FavoriteCity/FavoriteCity";
+import { setLoginAndPassword } from "features/login/login";
+import Button from "components/Button/Button";
 
 const Home: React.FC = () => {
   const [celsius, setCelsius] = useState(true);
   const [cityName, setCityName] = useState("");
 
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const handleLogout = () => {
     localStorage.setItem("user", "");
     navigate("/");
+    dispatch(
+      setLoginAndPassword({
+        login: "",
+        password: "",
+      })
+    );
   };
 
   if (localStorage.getItem("user") === "") {
@@ -25,18 +35,15 @@ const Home: React.FC = () => {
 
   return (
     <div className={styles.home}>
-      <div>
-        <button onClick={handleLogout}>Log Out</button>
-        <button
-          className={styles.temperatureBtn}
-          onClick={() => setCelsius(!celsius)}
-        >
-          {celsius ? <Celsius /> : <Fahrenheit />}
-        </button>
-      </div>
+      <Button onClick={handleLogout} text="Log Out" />
+      <Button
+        onClick={() => setCelsius(!celsius)}
+        text={!celsius ? <Celsius /> : <Fahrenheit />}
+        variant="secondary"
+      />
       <div className={styles.weatherInfo}>
         <CityInput setCityName={setCityName} />
-        <Weather cityName={cityName} />
+        <Weather cityName={cityName} celsius={celsius} />
       </div>
       <div className={styles.favorites}>
         <FavoriteCity cityName="Warsaw" />
