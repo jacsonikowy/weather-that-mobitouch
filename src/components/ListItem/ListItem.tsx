b3a4ee165709d087bd0bc3573463d1e0f0f4f007
jacsonikowy <michal6343@wp.pl>
2023-02-23 14:51:08 +0100

    style: css styling

--- src/components/ListItem/ListItem.tsx
+++ src/components/ListItem/ListItem.tsx
@@ -1,12 +1,21 @@
-import React from "react";
+import React, { ButtonHTMLAttributes, InputHTMLAttributes } from "react";
 import styles from "./ListItem.module.scss";
 
-interface ListItemProps {
-  text: any;
+interface ListItemProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  text: string;
+  country: string;
 }
 
-const ListItem: React.FC<ListItemProps> = ({ text }) => {
-  return <div className={styles.listItem}>{text}</div>;
+const ListItem: React.FC<ListItemProps> = ({ text, country, ...props }) => {
+  return (
+    <div className={styles.listItem}>
+      <p>{text}</p>
+      <div className={styles.listItemEnd}>
+        <p>{country}</p>
+        <button {...props} />
+      </div>
+    </div>
+  );
 };
 
 export default ListItem;
