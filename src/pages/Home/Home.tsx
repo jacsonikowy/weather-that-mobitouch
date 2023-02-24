b38ceb6fc3fb1fd027d2a193e72d2e2dd983ecc4
jacsonikowy <michal6343@wp.pl>
2023-02-24 15:23:47 +0100

    fix(home): add to redux cities from localStorage

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,7 +1,7 @@
 import CityInput from "components/CityInput/CityInput";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import styles from "./Home.module.scss";
 
 import { ReactComponent as Celsius } from "assets/icons/celsius.svg";
@@ -10,12 +10,20 @@ import Weather from "components/Weather/Weather";
 import FavoriteCity from "components/FavoriteCity/FavoriteCity";
 import { setLoginAndPassword } from "features/login/login";
 import Button from "components/Button/Button";
+import { RootState } from "store";
+import Modal from "components/Modal/Modal";
+import { WeatherDataProps } from "constants/WeatherDataProps";
+import { setFavoriteCity } from "features/favoriteCities/favoriteCities";
 
 const Home: React.FC = () => {
   const [celsius, setCelsius] = useState(true);
+  const [modalActive, setModalActive] = useState(false);
+  const [cityInModal, setCityInModal] = useState<WeatherDataProps>();
 
   const navigate = useNavigate();
   const dispatch = useDispatch();
+
+  const favoriteCities = useSelector((state: RootState) => state.favorites);
 
   const handleLogout = () => {
     localStorage.setItem("user", "");
@@ -27,6 +35,16 @@ const Home: React.FC = () => {
       })
     );
   };
+
+  useEffect(() => {
+    const favoriteCitiesLocalStorage = localStorage.getItem("favoriteCities");
+    if (favoriteCitiesLocalStorage) {
+      const citiesArray = JSON.parse(favoriteCitiesLocalStorage);
+      citiesArray.forEach((city: WeatherDataProps) => {
+        dispatch(setFavoriteCity(city));
+      });
+    }
+  }, [dispatch]);
 
   if (localStorage.getItem("user") === "") {
     return <div>UNAUTHORIZED</div>;
@@ -42,11 +60,32 @@ const Home: React.FC = () => {
       />
       <div className={styles.weatherInfo}>
         <CityInput />
-        <Weather celsius={celsius} />
+        <Weather
+          celsius={celsius}
+          setModalActive={setModalActive}
+          setCityInModal={setCityInModal}
+        />
       </div>
       <div className={styles.favorites}>
-        <FavoriteCity cityName="Warsaw" />
+        {favoriteCities.map((favoriteCity: WeatherDataProps) => {
+          return (
+            <FavoriteCity
+              name={favoriteCity.name}
+              weather={favoriteCity.weather}
+              main={favoriteCity.main}
+            />
+          );
+        })}
       </div>
+      {modalActive && cityInModal ? (
+        <Modal
+          active={modalActive}
+          cityInModal={cityInModal}
+          setModalActive={setModalActive}
+        />
+      ) : (
+        ""
+      )}
     </div>
   );
 };
