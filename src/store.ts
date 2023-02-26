20e05bf342c08d4f10f0d4484dc79eb938e57852
jacsonikowy <michal6345@wp.pl>
2023-02-26 17:50:11 +0100

    feat(store): new reducer isCelsius

--- src/store.ts
+++ src/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from "@reduxjs/toolkit";
 import LoginReducer from "features/login/login";
 import CityReducer from "features/cities/weather";
 import FavoriteReducer from "features/favoriteCities/favoriteCities";
+import isCelsiusReducer from 'features/isCelsius/isCelsius'
 
 export const store = configureStore({
   reducer: {
     login: LoginReducer,
     cityProps: CityReducer,
     favorites: FavoriteReducer,
+    isCelsius: isCelsiusReducer,
   },
 });
 
