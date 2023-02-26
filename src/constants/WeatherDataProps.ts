ac17752917391b3820b9bd2a76b0e9a8cb6e0d44
jacsonikowy <michal6345@wp.pl>
2023-02-26 17:55:16 +0100

    feat(WeatherDataProps): new data inside interface

--- src/constants/WeatherDataProps.ts
+++ src/constants/WeatherDataProps.ts
@@ -3,6 +3,13 @@ export interface WeatherDataProps {
   main: {
     temp: number;
     pressure: number;
+    feels_like: number;
+    humidity: number;
+    temp_min: number;
+    temp_max: number;
+  };
+  wind: {
+    speed: number;
   };
   name: string;
 }
