e299d5e841cc81d4e159061d6cdaad8e82a9bf8c
jacsonikowy <michal6345@wp.pl>
2023-02-26 17:55:47 +0100

    fix(FavoriteCity): handling removing

--- src/components/FavoriteCity/FavoriteCity.tsx
+++ src/components/FavoriteCity/FavoriteCity.tsx
@@ -3,20 +3,62 @@ import styles from "./FavoriteCity.module.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faStar } from "@fortawesome/free-solid-svg-icons";
 import { WeatherDataProps } from "constants/WeatherDataProps";
+import { showFahrenheitOrCelsius } from 'utils'
 
-const FavoriteCity: React.FC<WeatherDataProps> = ({ name, weather, main }) => {
+import { RootState } from "store";
+import { useDispatch, useSelector } from "react-redux";
+import { setFavoriteCityArray } from "features/favoriteCities/favoriteCities";
+import Button from "components/Button/Button";
+
+interface FavoriteCityProps {
+  favoriteCity: WeatherDataProps;
+}
+
+const FavoriteCity: React.FC<FavoriteCityProps> = ({ favoriteCity }) => {
+  const state = useSelector((state: RootState) => state.favorites.favorites);
+  const stateIsCelsius = useSelector((state: RootState) => state.isCelsius.isCelsius)
+  const dispatch = useDispatch();
+
+  const handleRemoveFromFavorites = (favoriteCity: WeatherDataProps) => {
+    if (favoriteCity !== null) {
+      const newFavoriteCityState = state.filter(
+        (city) => city.name !== favoriteCity.name
+      );
+      console.log(newFavoriteCityState)
+      dispatch(setFavoriteCityArray(newFavoriteCityState))
+
+      const favoriteCitiesFromLocalStorage =
+        localStorage.getItem("favoriteCities");
+      if (favoriteCitiesFromLocalStorage !== null) {
+        const parsedCities = JSON.parse(favoriteCitiesFromLocalStorage);
+        const result1 = parsedCities.filter(
+          (city: WeatherDataProps) => city.name !== favoriteCity.name
+        );
+
+        localStorage.setItem("favoriteCities", JSON.stringify(result1));
+      }
+    }
+  };
+
+  if (favoriteCity.weather === undefined && favoriteCity.main === undefined) {
+    return <></>;
+  }
+
   return (
     <div className={styles.favoriteCity}>
       <div className={styles.title}>
-        <span>{name}</span>
-        <FontAwesomeIcon icon={faStar} />
+        <span>{favoriteCity.name}</span>
+        <Button
+          text={<FontAwesomeIcon icon={faStar} />}
+          onClick={() => handleRemoveFromFavorites(favoriteCity)}
+        />
       </div>
       <div className={styles.informations}>
         <img
-          src={`http://openweathermap.org/img/wn/${weather[0].icon}@4x.png`}
+          src={`http://openweathermap.org/img/wn/${favoriteCity.weather[0].icon}@4x.png` || ""}
           alt="weatherIcon"
         />
-        {main.temp}&deg;C {main.pressure}
+        {showFahrenheitOrCelsius(favoriteCity.main.temp, stateIsCelsius)} {favoriteCity.main.pressure}
       </div>
     </div>
   );
