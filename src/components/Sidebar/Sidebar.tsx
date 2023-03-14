b820d00537fe0683e28de3345a5ce6293132686a
jacsonikowy <michal6345@wp.pl>
2023-03-14 21:53:04 +0100

    feat: add navigate from SidebarData location

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -5,16 +5,28 @@ import Item from "components/Item/Item";
 import styles from "./Sidebar.module.scss"
 import { logout } from "utils";
 import { useNavigate } from "react-router";
+import Button from "components/Button/Button"
+import { faX } from '@fortawesome/free-solid-svg-icons'
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 const Sidebar: React.FC = () => {
 
+    const [active, setActive] = useState(false);
     const navigate = useNavigate();
 
+
+
     return (
-        <div className={styles.sidebar}>
+        <div className={`${styles.container} ${active ? "" : styles.nobackground}`}>
+        <div className={`${styles.sidebar} ${active ? styles.active : ""}`}>
             <div className={styles.panels}>
                 {sidebarData.map((item) => {
-                    return (<Item img={item.img} text={item.text} />)
+                    return (<Item img={item.img} text={item.text} onClick={() => {
+                      if(!!item.location){
+                        navigate(item.location)
+                      }  
+                    }}/>)
                 })}
             </div>
             <div className={styles.panelsSystem}>
@@ -23,6 +35,10 @@ const Sidebar: React.FC = () => {
                     return <Item img={item.img} text={item.text} isLogout={item.isLogout} onClick={item.isLogout ? () => logout(navigate) : undefined}/>
                 })}
             </div>
+        </div>
+        <div className={styles.closeBtnWrapper}>
+            <Button text={<FontAwesomeIcon icon={ active ? faX : faBars} />} onClick={() => setActive(!active)}></Button>
+        </div> 
         </div>
     )
 }
