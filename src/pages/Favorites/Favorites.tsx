ebb5dad9a8968d9ac4167642ab6532a0698ed1e1
jacsonikowy <michal6345@wp.pl>
2023-03-15 22:34:08 +0100

    feat(pages): render favorite cities inside page

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -1,30 +1,54 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Sidebar from 'components/Sidebar/Sidebar'
 import styles from './Favorites.module.scss';
 import { useSelector } from 'react-redux';
 import { RootState } from 'store';
 import FavoritePanel from 'components/FavoritePanel/FavoritePanel';
 import Modal from 'components/Modal/Modal';
+import Button from 'components/Button/Button';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons'
+import { useDispatch } from 'react-redux';
+import { setSidebarActive } from 'features/sidebar/sidebar';
+import { fetchWeatherData } from 'utils';
+import { WeatherDataProps } from 'constants/WeatherDataProps';
+import cityInModal, { setCityInModal, setModalActive } from 'features/cityInModal/cityInModal';
 
 const Favorites: React.FC = () => {
 
+    const [weatherData, setWeatherData] = useState<WeatherDataProps>();
+
     const favorites = useSelector((state: RootState) => state.favorites.favorites)
     const modalActive = useSelector((state: RootState) => state.cityInModal.activeModal)
-    console.log(favorites)
+    const city = useSelector((state: RootState) => state.cityProps.cityProps);
+    const cityInModal = useSelector((state: RootState) => state.cityInModal.cityInModal)
+
+    const dispatch = useDispatch();
 
     return (
         <div className={styles.favorites}>
             <Sidebar />
+            <div className={`${styles.closeBtnWrapper}`}>
+                <Button text={<FontAwesomeIcon icon={ faBars }/>} onClick={() => dispatch(setSidebarActive(true))}></Button>
+            </div> 
             <div className={styles.content}>
-                {favorites.length === 0 ? "There are no Favorites!" : 
+                {favorites.length === 0 ? <span>There are no Favorites!</span> : 
                <div className={styles.favoritePanels}>
                     <div>
-                        <FavoritePanel name="Warsaw" weatherImg="10n" temp={10} pressure={997} />
+
+                        {favorites.map(city => {
+                            console.log(city)
+                            return <FavoritePanel name={city.name} weatherImg={city.weather[0].icon} pressure={city.main.pressure} temp={city.main.temp} onClick={() =>{
+                             dispatch(setCityInModal(city))   
+                             dispatch(setModalActive(true))
+                            }}/>
+                        })}
+
                     </div>
                 </div>
                 }
             </div>
-            {modalActive && <Modal />}
+            {modalActive && cityInModal && <Modal />}
         </div>
     )
 }
