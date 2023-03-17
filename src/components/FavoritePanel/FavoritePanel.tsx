afe031b792125d3928ba570fb1b666c37a867c91
jacsonikowy <michal6345@wp.pl>
2023-03-17 22:25:46 +0100

    fix(FavoritePanel): add button, styling

--- src/components/FavoritePanel/FavoritePanel.tsx
+++ src/components/FavoritePanel/FavoritePanel.tsx
@@ -1,30 +1,53 @@
 import React from 'react'
 import styles from './FavoritePanel.module.scss'
 import { FavoriteState } from 'constants/FavoriteState'
-import { displayIcon } from 'utils'
+import { convertToFahrenheit, displayIcon } from 'utils'
 import Button from 'components/Button/Button'
-import { useDispatch } from 'react-redux'
-import { setModalActive } from 'features/cityInModal/cityInModal'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootState } from 'store'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+import { handleRemoveFromFavorites } from 'utils'
+import { WeatherDataProps } from 'constants/WeatherDataProps'
 
 interface FavoritePanelProps extends FavoriteState {
     onClick: () => void
+    weatherData: WeatherDataProps
 }
 
 
-const FavoritePanel: React.FC<FavoritePanelProps> = ({name, weatherImg, temp, pressure, onClick}) => {
+const FavoritePanel: React.FC<FavoritePanelProps> = ({name, weatherImg, temp, pressure, weatherData, onClick}) => {
+
+    const celsius = useSelector((state: RootState) => state.isCelsius.isCelsius)
+    const favoriteCities = useSelector((state: RootState) => state.favorites.favorites)
 
     const dispatch = useDispatch();
 
+
     return (
-        <div className={styles.favoritePanel}>
-            <div className={styles.wrapperSvg}>
+        <tr className={styles.favoritePanel}>
+            <td>
+                <Button
+                text={
+                    <FontAwesomeIcon
+                    icon={ faStar }
+                    />
+                }
+                onClick={() =>
+                    {
+                    handleRemoveFromFavorites(dispatch, weatherData, favoriteCities)
+                    }
+                }
+                />
+            </td>
+            <td className={styles.wrapperSvg}>
                 {weatherImg ? displayIcon(weatherImg) : "" }
-            </div>
-            <span>{name}</span>
-            <span>{temp} C</span>
-            <span>{pressure} hPa</span>
+            </td>
+            <td>{name}</td>
+            <td>{celsius ? `${Math.round(temp)}°C` : `${convertToFahrenheit(temp)}°F`}</td>
+            <td>{pressure} hPa</td>
             <Button text="See more" onClick={onClick}/>
-        </div>
+        </tr>
     )
 }
 
