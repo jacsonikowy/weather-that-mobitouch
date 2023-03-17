8245d9dafce7b626fa5d1dd53596e66c3d74d4fe
jacsonikowy <michal6345@wp.pl>
2023-03-17 22:21:03 +0100

    fix(pages): change from div to tables

--- src/pages/Favorites/Favorites.tsx
+++ src/pages/Favorites/Favorites.tsx
@@ -10,20 +10,28 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons'
 import { useDispatch } from 'react-redux';
 import { setSidebarActive } from 'features/sidebar/sidebar';
-import { fetchWeatherData } from 'utils';
-import { WeatherDataProps } from 'constants/WeatherDataProps';
-import cityInModal, { setCityInModal, setModalActive } from 'features/cityInModal/cityInModal';
+import { setCityInModal, setModalActive } from 'features/cityInModal/cityInModal';
+import { setFavoriteCityArray } from 'features/favoriteCities/favoriteCities';
+import { ReactComponent as Celsius } from 'assets/icons/celsius.svg'
+import { ReactComponent as Fahrenheit } from 'assets/icons/fahrenheit.svg'
+import { setIsCelsius } from 'features/isCelsius/isCelsius';
 
 const Favorites: React.FC = () => {
 
-    const [weatherData, setWeatherData] = useState<WeatherDataProps>();
-
     const favorites = useSelector((state: RootState) => state.favorites.favorites)
     const modalActive = useSelector((state: RootState) => state.cityInModal.activeModal)
-    const city = useSelector((state: RootState) => state.cityProps.cityProps);
     const cityInModal = useSelector((state: RootState) => state.cityInModal.cityInModal)
+    const celsius = useSelector((state: RootState) => state.isCelsius.isCelsius)
 
     const dispatch = useDispatch();
+
+    useEffect(() => {
+        const favoriteCitiesLocalStorage = localStorage.getItem("favoriteCities");
+        if (favoriteCitiesLocalStorage) {
+            const citiesArray = JSON.parse(favoriteCitiesLocalStorage);
+            dispatch(setFavoriteCityArray(citiesArray));
+        }
+    }, [])
 
     return (
         <div className={styles.favorites}>
@@ -31,21 +39,25 @@ const Favorites: React.FC = () => {
             <div className={`${styles.closeBtnWrapper}`}>
                 <Button text={<FontAwesomeIcon icon={ faBars }/>} onClick={() => dispatch(setSidebarActive(true))}></Button>
             </div> 
+            <div className={styles.btnsWrapper}>
+            <Button
+                onClick={() => dispatch(setIsCelsius(!celsius))}
+                text={!celsius ? <Celsius /> : <Fahrenheit />}
+                variant="secondary"
+            />
+            </div>
             <div className={styles.content}>
-                {favorites.length === 0 ? <span>There are no Favorites!</span> : 
-               <div className={styles.favoritePanels}>
-                    <div>
-
+                {favorites.length !== 0 ?
+               <table className={styles.favoritePanels}>
                         {favorites.map(city => {
                             console.log(city)
-                            return <FavoritePanel name={city.name} weatherImg={city.weather[0].icon} pressure={city.main.pressure} temp={city.main.temp} onClick={() =>{
+                            return <FavoritePanel name={city.name} weatherImg={city.weather[0].icon} pressure={city.main.pressure} temp={city.main.temp} weatherData={city} onClick={() =>{
                              dispatch(setCityInModal(city))   
                              dispatch(setModalActive(true))
                             }}/>
                         })}
 
-                    </div>
-                </div>
+                </table> : <span>asdas</span>
                 }
             </div>
             {modalActive && cityInModal && <Modal />}
