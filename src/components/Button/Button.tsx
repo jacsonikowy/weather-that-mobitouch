998235b953eaa79084dae7903067a68642d69f31
jacsonikowy <michal6345@wp.pl>
2023-03-18 17:57:14 +0100

    feat: add button for celsius/fahrenheit

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,15 +1,45 @@
 import React, { ButtonHTMLAttributes } from "react";
 import styles from "./Button.module.scss";
+import { toast } from "react-toastify";
+import { useSelector } from "react-redux";
+import { RootState } from "store";
+import { ReactComponent as Celsius } from "assets/icons/celsius.svg";
+import { ReactComponent as Fahrenheit } from "assets/icons/fahrenheit.svg";
+import { useDispatch } from "react-redux";
+import { setIsCelsius } from "features/isCelsius/isCelsius";
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
-  text: string | React.ReactNode;
-  variant?: "secondary";
+  text?: string | React.ReactNode;
+  variant?: "secondary" | "third";
 }
 
 const Button: React.FC<ButtonProps> = ({ text, variant, ...props }) => {
+  const celsius = useSelector((state: RootState) => state.isCelsius.isCelsius);
+
+  const dispatch = useDispatch();
+
+  if (variant === "secondary") {
+    const notifyChangeTemp = () => {
+      toast(`Changed to ${celsius ? "Fahrenheit" : "Celsius"}`);
+      dispatch(setIsCelsius(!celsius));
+    };
+
+    return (
+      <button
+        className={`${styles.button} ${styles.secondary} `}
+        onClick={() => notifyChangeTemp()}
+      >
+        {celsius ? <Fahrenheit /> : <Celsius />}
+      </button>
+    );
+  }
+
   return (
-    <button className={`${styles.button} ${variant === "secondary" ? styles.secondary : ""}`} {...props}>
-      <p>{text}</p>
+    <button
+      className={`${styles.button} ${variant ? styles.seeMore : ""}`}
+      {...props}
+    >
+      {text}
     </button>
   );
 };
