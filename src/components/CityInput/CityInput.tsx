9d09923f6f6da9a52f79ab90a01ba0fed78e93b5
jacsonikowy <michal6345@wp.pl>
2023-03-31 21:42:38 +0200

    fix: setting favorie cities

--- src/components/CityInput/CityInput.tsx
+++ src/components/CityInput/CityInput.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "./CityInput.module.scss";
 import { useDebounce } from "hooks/UseDebounce";
 import ListItem from "components/ListItem/ListItem";
@@ -6,39 +6,47 @@ import ListItem from "components/ListItem/ListItem";
 import { useDispatch } from "react-redux";
 import { setCity } from "features/cities/weather";
 import { StateProps } from "constants/StateProps";
-import { ReactComponent as Glass } from 'assets/icons/glass.svg'
+import { ReactComponent as Glass } from "assets/icons/glass.svg";
 import { useGetFavoriteCityQuery } from "services/getFavoriteCity";
 
 const CityInput: React.FC = () => {
   const [cityInputName, setCityInputName] = useState("");
+  const [cities, setCities] = useState<StateProps[]>([]);
   const debouncedValue = useDebounce<string>(cityInputName, 300);
   const dispatch = useDispatch();
 
   const handleDispatch = (city: StateProps) => {
     dispatch(setCity(city));
     setCityInputName("");
+    setCities([]);
   };
 
   const { data, error, isLoading } = useGetFavoriteCityQuery(debouncedValue, {
     skip: debouncedValue.length === 0,
   });
 
+  useEffect(() => {
+    if (data) {
+      setCities(data);
+    }
+  }, [data]);
+
   return (
     <div className={styles.cityInputDiv}>
       <div className={styles.wrapperInputAndBtn}>
         <label htmlFor="">
           <Glass />
-        <input
-          type="text"
-          className={styles.inputCity}
-          onChange={(e) => setCityInputName(e.target.value)}
-          value={cityInputName}
-        />
+          <input
+            type="text"
+            className={styles.inputCity}
+            onChange={(e) => setCityInputName(e.target.value)}
+            value={cityInputName}
+          />
         </label>
-        {data
-          ? data.length === 0
+        {cities
+          ? cities.length === 0
             ? ""
-            : data.map((city: StateProps, index: number) => {
+            : cities.map((city: StateProps, index: number) => {
                 return (
                   <ListItem
                     key={index}
